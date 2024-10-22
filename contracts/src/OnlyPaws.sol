a8d0dcf71b62ffb03efcb0083e77c385b040dc75
Larry the Cucumber <larrythecucumber321@gmail.com>
2024-10-22 10:43:49 -0700

    contract changes

--- contracts/src/OnlyPaws.sol
+++ contracts/src/OnlyPaws.sol
@@ -1,15 +1,48 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "./interfaces/IBerachainRewardsVault.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "forge-std/console.sol";
 
-contract OnlyPaws is Ownable {
-    IBerachainRewardsVault public immutable rewardsVault;
-    IERC20 public immutable bgtToken;
+interface IBGT is IERC20 {
+    function redeem(address receiver, uint256 amount) external;
+}
+
+interface IBerachainRewardsVaultFactory {
+    function createRewardsVault(
+        address stakingToken
+    ) external returns (address);
+}
+
+interface IBerachainRewardsVault {
+    function stake(uint256 amount) external;
+    function getReward(address account) external returns (uint256);
+}
+
+contract PawStakingToken is ERC20 {
+    address public immutable onlyPaws;
+
+    constructor(address _onlyPaws) ERC20("Paw Staking Token", "PST") {
+        onlyPaws = _onlyPaws;
+    }
+
+    function mint(address to, uint256 amount) external {
+        require(msg.sender == onlyPaws, "Only onlyPaws can mint");
+        _mint(to, amount);
+    }
+}
+
+contract OnlyPaws {
+    using SafeERC20 for IERC20;
+
+    IBerachainRewardsVault public immutable vault;
+    PawStakingToken public immutable stakingToken;
+    IBGT public constant bgt = IBGT(0xbDa130737BDd9618301681329bF2e46A016ff9Ad);
 
     uint256 public constant REWARD_DURATION = 7 days;
-    uint256 public constant REWARD_AMOUNT = 100 * 1e18; // 100 BGT tokens
+    uint256 public constant REWARD_PRECISION = 1e18;
 
     struct PawImage {
         address owner;
@@ -17,9 +50,24 @@ contract OnlyPaws is Ownable {
         bool isForSale;
     }
 
+    struct UserStake {
+        uint256 amount;
+        uint256 timestamp;
+    }
+
     mapping(uint256 => PawImage) public pawImages;
     mapping(address => uint256[]) public userPaws;
-    mapping(uint256 => uint256) public pawPurchaseTime;
+    mapping(address => mapping(uint256 => UserStake)) public userStakes;
+    mapping(uint256 => uint256) public pawLastPurchaseTime;
+
+    uint256 public totalSupply;
+    uint256 public lastUpdateTime;
+    uint256 public rewardRate;
+    uint256 public rewardPerTokenStored;
+    uint256 public finishAt;
+
+    mapping(address => uint256) public userRewardPerTokenPaid;
+    mapping(address => uint256) public rewards;
 
     event PawImageAdded(
         uint256 indexed pawId,
@@ -27,20 +75,29 @@ contract OnlyPaws is Ownable {
         uint256 price
     );
     event PawPurchased(address indexed buyer, uint256 indexed pawId);
+    event RewardsDistributed(uint256 amount);
     event RewardsClaimed(address indexed user, uint256 amount);
 
-    constructor(address _rewardsVault, address _bgtToken) {
-        rewardsVault = IBerachainRewardsVault(_rewardsVault);
-        bgtToken = IERC20(_bgtToken);
+    constructor() {
+        stakingToken = new PawStakingToken(address(this));
+        IBerachainRewardsVaultFactory factory = IBerachainRewardsVaultFactory(
+            0x2B6e40f65D82A0cB98795bC7587a71bfa49fBB2B
+        );
+        vault = IBerachainRewardsVault(
+            factory.createRewardsVault(address(stakingToken))
+        );
+        stakingToken.mint(address(this), 1e18);
+        stakingToken.approve(address(vault), 1e18);
+        vault.stake(1e18);
     }
 
     function addPawImage(uint256 pawId, uint256 price) external {
-        require(
-            pawImages[pawId].owner == address(0),
-            "Paw image already exists"
-        );
-        pawImages[pawId] = PawImage(msg.sender, price, true);
-        userPaws[msg.sender].push(pawId);
+        require(pawImages[pawId].owner == address(0), "Paw already exists");
+        pawImages[pawId] = PawImage({
+            owner: msg.sender,
+            price: price,
+            isForSale: true
+        });
         emit PawImageAdded(pawId, msg.sender, price);
     }
 
@@ -49,41 +106,135 @@ contract OnlyPaws is Ownable {
         require(paw.isForSale, "Paw not for sale");
         require(msg.value == paw.price, "Incorrect payment amount");
 
-        address seller = paw.owner;
+        address payable seller = payable(paw.owner);
         paw.owner = msg.sender;
         paw.isForSale = false;
         userPaws[msg.sender].push(pawId);
-        pawPurchaseTime[pawId] = block.timestamp;
 
-        payable(seller).transfer(msg.value);
+        updateReward(msg.sender);
+        totalSupply++;
+        pawLastPurchaseTime[pawId] = block.timestamp;
+        userStakes[msg.sender][pawId] = UserStake({
+            amount: 1,
+            timestamp: block.timestamp
+        });
+
+        (bool success, ) = seller.call{value: paw.price}("");
+        require(success, "BERA transfer failed");
         emit PawPurchased(msg.sender, pawId);
     }
 
-    function claimRewards() external {
-        uint256 totalRewards = 0;
+    function harvestRewards() public {
+        uint256 bgtReward = vault.getReward(address(this));
+        console.log("BGT Reward received:", bgtReward);
+        console.log(
+            "Contract BGT balance before redeem:",
+            bgt.balanceOf(address(this))
+        );
+        if (bgtReward > 0) {
+            bgt.redeem(address(this), bgtReward);
+            uint256 nativeReward = address(this).balance;
+            console.log("Native reward after redeem:", nativeReward);
+            notifyRewardAmount(nativeReward);
+        }
+        console.log(
+            "Contract BGT balance after redeem:",
+            bgt.balanceOf(address(this))
+        );
+    }
 
-        for (uint256 i = 0; i < userPaws[msg.sender].length; i++) {
-            uint256 pawId = userPaws[msg.sender][i];
-            uint256 purchaseTime = pawPurchaseTime[pawId];
+    function notifyRewardAmount(uint256 reward) internal {
+        console.log("Notifying reward amount:", reward);
+        if (block.timestamp >= finishAt) {
+            rewardRate = reward / REWARD_DURATION;
+        } else {
+            uint256 remainingRewards = (finishAt - block.timestamp) *
+                rewardRate;
+            rewardRate = (reward + remainingRewards) / REWARD_DURATION;
+        }
+        lastUpdateTime = block.timestamp;
+        finishAt = block.timestamp + REWARD_DURATION;
+        console.log("New reward rate:", rewardRate);
+        console.log("New finish time:", finishAt);
+        emit RewardsDistributed(reward);
+    }
 
-            if (block.timestamp - purchaseTime <= REWARD_DURATION) {
-                totalRewards += REWARD_AMOUNT;
+    function updateExpiredStakes(address account) internal {
+        uint256[] storage pawIds = userPaws[account];
+        for (uint256 i = 0; i < pawIds.length; i++) {
+            uint256 pawId = pawIds[i];
+            UserStake storage stake = userStakes[account][pawId];
+            if (
+                stake.amount > 0 &&
+                block.timestamp > stake.timestamp + REWARD_DURATION
+            ) {
+                totalSupply--;
+                stake.amount = 0;
             }
         }
-
-        require(totalRewards > 0, "No rewards to claim");
-
-        rewardsVault.delegateStake(msg.sender, totalRewards);
-        emit RewardsClaimed(msg.sender, totalRewards);
     }
 
-    function getUserPaws(
-        address user
-    ) external view returns (uint256[] memory) {
-        return userPaws[user];
+    function lastTimeRewardApplicable() public view returns (uint256) {
+        return _min(finishAt, block.timestamp);
     }
 
-    function withdrawFunds() external onlyOwner {
-        payable(owner()).transfer(address(this).balance);
+    function rewardPerToken() public view returns (uint256) {
+        if (totalSupply == 0) {
+            return rewardPerTokenStored;
+        }
+        return
+            rewardPerTokenStored +
+            (rewardRate *
+                (lastTimeRewardApplicable() - lastUpdateTime) *
+                REWARD_PRECISION) /
+            totalSupply;
     }
+
+    function earned(address account) public view returns (uint256) {
+        uint256 activeStakes = 0;
+        uint256[] storage pawIds = userPaws[account];
+        for (uint256 i = 0; i < pawIds.length; i++) {
+            UserStake storage stake = userStakes[account][pawIds[i]];
+            if (
+                stake.amount > 0 &&
+                block.timestamp <= stake.timestamp + REWARD_DURATION
+            ) {
+                activeStakes++;
+            }
+        }
+        return
+            ((activeStakes *
+                (rewardPerToken() - userRewardPerTokenPaid[account])) /
+                REWARD_PRECISION) + rewards[account];
+    }
+
+    function claimRewards() external {
+        harvestRewards();
+        updateReward(msg.sender);
+        uint256 reward = rewards[msg.sender];
+        console.log("Pending rewards to claim:", reward);
+        if (reward > 0) {
+            rewards[msg.sender] = 0;
+            (bool success, ) = payable(msg.sender).call{value: reward}("");
+            require(success, "BERA transfer failed");
+            emit RewardsClaimed(msg.sender, reward);
+        }
+    }
+
+    function updateReward(address account) internal {
+        rewardPerTokenStored = rewardPerToken();
+        lastUpdateTime = lastTimeRewardApplicable();
+
+        if (account != address(0)) {
+            rewards[account] = earned(account);
+            userRewardPerTokenPaid[account] = rewardPerTokenStored;
+            updateExpiredStakes(account);
+        }
+    }
+
+    function _min(uint256 x, uint256 y) private pure returns (uint256) {
+        return x <= y ? x : y;
+    }
+
+    receive() external payable {}
 }
--- contracts/test/OnlyPaws.t.sol
+++ contracts/test/OnlyPaws.t.sol
@@ -1,78 +1,216 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
 import "forge-std/Test.sol";
 import "../src/OnlyPaws.sol";
-import "./mocks/MockERC20.sol";
-import "./mocks/MockRewardsVault.sol";
+import "forge-std/console.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+// Updated Mock BGT Token
+contract MockBGT is ERC20 {
+    constructor() ERC20("Mock BGT", "MBGT") {}
+
+    function mint(address account, uint256 amount) public {
+        _mint(account, amount);
+    }
+
+    function redeem(address receiver, uint256 amount) public {
+        require(balanceOf(msg.sender) >= amount, "Insufficient BGT balance");
+        _burn(msg.sender, amount);
+        console.log("BGT redeemed. Sender:", msg.sender);
+        console.log("BGT redeemed. Amount:", amount);
+        console.log("BGT redeemed. Receiver:", receiver);
+        console.log("BGT balance after redeem:", balanceOf(msg.sender));
+        payable(receiver).transfer(amount);
+    }
+
+    receive() external payable {}
+}
+
+// Mock Rewards Vault
+contract MockRewardsVault is IBerachainRewardsVault {
+    MockBGT public bgtToken;
+    uint256 public stakedAmount;
+    uint256 public constant REWARD_RATE = 1e15; // 0.001 BGT per second
+
+    constructor(address _bgtToken) {
+        bgtToken = MockBGT(payable(_bgtToken));
+    }
+
+    function stake(uint256 amount) external {
+        stakedAmount += amount;
+    }
+
+    function getReward(address account) external returns (uint256) {
+        uint256 reward = REWARD_RATE * 7 days; // 1 week of rewards
+        bgtToken.transfer(account, reward);
+        return reward;
+    }
+
+    receive() external payable {}
+}
+
+// Mock Rewards Vault Factory
+contract MockRewardsVaultFactory is IBerachainRewardsVaultFactory {
+    address public immutable rewardsVault;
+
+    constructor(address _rewardsVault) {
+        rewardsVault = _rewardsVault;
+    }
+
+    function createRewardsVault(
+        address stakingToken
+    ) external returns (address) {
+        return rewardsVault;
+    }
+}
 
 contract OnlyPawsTest is Test {
     OnlyPaws public onlyPaws;
-    MockERC20 public bgtToken;
+    MockBGT public bgtToken;
     MockRewardsVault public rewardsVault;
+    MockRewardsVaultFactory public rewardsVaultFactory;
 
     address public user1 = address(0x1);
     address public user2 = address(0x2);
+    address public user3 = address(0x3);
+
+    uint256 constant INITIAL_BALANCE = 1000 ether;
+    uint256 constant PAW_PRICE = 0.1 ether;
+    uint256 constant WEEKLY_REWARD = 100 ether;
+
+    receive() external payable {}
+
+    address constant BGT_ADDRESS = 0xbDa130737BDd9618301681329bF2e46A016ff9Ad;
 
     function setUp() public {
-        bgtToken = new MockERC20("BGT Token", "BGT", 18);
-        rewardsVault = new MockRewardsVault(address(bgtToken));
-        onlyPaws = new OnlyPaws(address(rewardsVault), address(bgtToken));
+        // Deploy MockBGT at the specific address
+        vm.etch(BGT_ADDRESS, address(new MockBGT()).code);
+        bgtToken = MockBGT(payable(BGT_ADDRESS));
 
-        vm.deal(user1, 1 ether);
-        vm.deal(user2, 1 ether);
+        rewardsVault = new MockRewardsVault(BGT_ADDRESS);
+        rewardsVaultFactory = new MockRewardsVaultFactory(
+            address(rewardsVault)
+        );
+
+        vm.etch(
+            0x2B6e40f65D82A0cB98795bC7587a71bfa49fBB2B,
+            address(rewardsVaultFactory).code
+        );
+
+        onlyPaws = new OnlyPaws();
+
+        vm.deal(user1, INITIAL_BALANCE);
+        vm.deal(user2, INITIAL_BALANCE);
+        vm.deal(user3, INITIAL_BALANCE);
+        vm.deal(address(this), INITIAL_BALANCE);
+        vm.deal(BGT_ADDRESS, INITIAL_BALANCE * 100); // Ensure MockBGT has enough ETH
+
+        bgtToken.mint(address(rewardsVault), WEEKLY_REWARD * 52);
+        bgtToken.mint(address(onlyPaws), WEEKLY_REWARD * 52 * 10);
+
+        console.log(
+            "RewardsVault BGT balance:",
+            bgtToken.balanceOf(address(rewardsVault))
+        );
+        console.log(
+            "OnlyPaws BGT balance:",
+            bgtToken.balanceOf(address(onlyPaws))
+        );
+
+        vm.startPrank(address(this));
+        onlyPaws.addPawImage(1, PAW_PRICE);
+        onlyPaws.addPawImage(2, PAW_PRICE);
+        onlyPaws.addPawImage(3, PAW_PRICE);
+        vm.stopPrank();
     }
 
-    function testPurchasePaw() public {
+    function testRewardDistribution() public {
         vm.prank(user1);
-        onlyPaws.purchasePaw{value: 0.1 ether}(1);
-
-        assertEq(onlyPaws.pawOwners(1), user1);
-        assertEq(onlyPaws.getUserPaws(user1)[0], 1);
-    }
-
-    function testCannotPurchaseSamePawTwice() public {
-        vm.prank(user1);
-        onlyPaws.purchasePaw{value: 0.1 ether}(1);
-
-        vm.prank(user2);
-        vm.expectRevert("Paw already purchased");
-        onlyPaws.purchasePaw{value: 0.1 ether}(1);
-    }
-
-    function testClaimRewards() public {
-        vm.prank(user1);
-        onlyPaws.purchasePaw{value: 0.1 ether}(1);
+        onlyPaws.purchasePaw{value: PAW_PRICE}(1);
 
         vm.warp(block.timestamp + 3 days);
-
-        vm.prank(user1);
-        onlyPaws.claimRewards();
-
-        assertEq(rewardsVault.delegatedStake(user1), 100 * 1e18);
-    }
-
-    function testCannotClaimRewardsAfterDuration() public {
-        vm.prank(user1);
-        onlyPaws.purchasePaw{value: 0.1 ether}(1);
-
-        vm.warp(block.timestamp + 8 days);
-
-        vm.prank(user1);
-        vm.expectRevert("No rewards to claim");
-        onlyPaws.claimRewards();
-    }
-
-    function testWithdrawFunds() public {
-        vm.prank(user1);
-        onlyPaws.purchasePaw{value: 0.1 ether}(1);
+        onlyPaws.harvestRewards();
 
         vm.prank(user2);
-        onlyPaws.purchasePaw{value: 0.1 ether}(2);
+        onlyPaws.purchasePaw{value: PAW_PRICE}(2);
 
-        uint256 initialBalance = address(this).balance;
-        onlyPaws.withdrawFunds();
-        uint256 finalBalance = address(this).balance;
+        vm.warp(block.timestamp + 4 days);
+        onlyPaws.harvestRewards();
 
-        assertEq(finalBalance - initialBalance, 0.2 ether);
+        uint256 user1BalanceBefore = user1.balance;
+        vm.prank(user1);
+        onlyPaws.claimRewards();
+        uint256 user1Rewards = user1.balance - user1BalanceBefore;
+
+        uint256 user2BalanceBefore = user2.balance;
+        vm.prank(user2);
+        onlyPaws.claimRewards();
+        uint256 user2Rewards = user2.balance - user2BalanceBefore;
+
+        console.log("User 1 rewards:", user1Rewards);
+        console.log("User 2 rewards:", user2Rewards);
+
+        assertGt(user1Rewards, user2Rewards);
+        assertApproxEqRel(user1Rewards, (user2Rewards * 7) / 4, 0.01e18);
+    }
+
+    function testRewardDistributionOverTime() public {
+        vm.prank(user1);
+        onlyPaws.purchasePaw{value: PAW_PRICE}(1);
+
+        vm.warp(block.timestamp + 1 days);
+        onlyPaws.harvestRewards();
+
+        vm.prank(user2);
+        onlyPaws.purchasePaw{value: PAW_PRICE}(2);
+
+        vm.warp(block.timestamp + 4 days);
+        onlyPaws.harvestRewards();
+
+        vm.prank(user3);
+        onlyPaws.purchasePaw{value: PAW_PRICE}(3);
+
+        vm.warp(block.timestamp + 3 days);
+        onlyPaws.harvestRewards();
+
+        uint256 user1BalanceBefore = user1.balance;
+        vm.prank(user1);
+        onlyPaws.claimRewards();
+        uint256 user1Rewards = user1.balance - user1BalanceBefore;
+
+        uint256 user2BalanceBefore = user2.balance;
+        vm.prank(user2);
+        onlyPaws.claimRewards();
+        uint256 user2Rewards = user2.balance - user2BalanceBefore;
+
+        uint256 user3BalanceBefore = user3.balance;
+        vm.prank(user3);
+        onlyPaws.claimRewards();
+        uint256 user3Rewards = user3.balance - user3BalanceBefore;
+
+        console.log("User 1 rewards:", user1Rewards);
+        console.log("User 2 rewards:", user2Rewards);
+        console.log("User 3 rewards:", user3Rewards);
+
+        assertGt(user1Rewards, user3Rewards);
+        assertGt(user2Rewards, user3Rewards);
+
+        uint256 totalDays = 8;
+        assertApproxEqRel(
+            user1Rewards,
+            (WEEKLY_REWARD * 8) / totalDays,
+            0.01e18
+        );
+        assertApproxEqRel(
+            user2Rewards,
+            (WEEKLY_REWARD * 7) / totalDays,
+            0.01e18
+        );
+        assertApproxEqRel(
+            user3Rewards,
+            (WEEKLY_REWARD * 3) / totalDays,
+            0.01e18
+        );
     }
 }
